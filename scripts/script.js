4fe025d3c66afa9c93ada0637bb16123fbf644ad
apupek <116450411+aporek@users.noreply.github.com>
2022-12-17 19:13:28 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+let swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+});
+
+
+
